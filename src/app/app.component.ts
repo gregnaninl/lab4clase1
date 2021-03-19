4764168cbc73def2be94dba575366a694aa75d16
Gregnanin Leandro <gregnanin.leandro@gmail.com>
2021-03-18 21:20:23 -0300

    Primer commit. Modificaciones para deploy heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto';
+  title = 'Ingrese las Edades';
+  edad1: number =0;
+  edad2: number =0;
+  suma: number | undefined ;
+  promedio: number | undefined ;
+
+  Calcular(){
+    this.suma= this.edad1 +this.edad2;
+    this.promedio = this.suma/2;
+
+  }
+
+  Limpiar(){
+    this.suma=0;
+    this.promedio=0;
+  }
+
+
 }
+
